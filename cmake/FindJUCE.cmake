13da057dd7045c8dacba90c174b28cbbbbc05693
Ehud Frank <ehud.frank@kvernelandgroup.com>
2026-06-07 11:51:36 +0200

    Add VSCode tasks for building and running the project; update JUCE fetch configuration

--- cmake/FindJUCE.cmake
+++ cmake/FindJUCE.cmake
@@ -14,7 +14,8 @@ if(NOT TARGET JUCE::JUCE)
     FetchContent_Declare(
       JUCE
       GIT_REPOSITORY https://github.com/juce-framework/JUCE.git
-      GIT_TAG 8.0.8)
+      GIT_TAG 8.0.8
+      GIT_SHALLOW TRUE)
     FetchContent_MakeAvailable(JUCE)
   endif()
 endif()
