8d17e422026e7542d76ddc91e9b6c3ecd26d30ef
Ehud Frank <ehud.frank@kvernelandgroup.com>
2026-09-11 09:32:12 +0200

    Fix Windows release workflow dependencies

--- cmake/FindCAN_Stack.cmake
+++ cmake/FindCAN_Stack.cmake
@@ -3,6 +3,6 @@ if(NOT TARGET isobus::isobus)
   FetchContent_Declare(
     CAN_Stack
     GIT_REPOSITORY https://github.com/ef12/AgIsoStack-plus-plus.git
-    GIT_TAG 86ed5e521b8d95433eb4688e2ded4d82c32cb383)
+    GIT_TAG 1eb0a89f21e0c2ea57a2c63b93a7d3b2218bc66f)
   FetchContent_MakeAvailable(CAN_Stack)
 endif()
